f1b37bcff0bc22821093178471b630071600e299
Jason Robert Fincham <JRFincham@yahoo.com>
2015-02-23 01:02:54 -0500

    router files added

--- app/scripts/routers/main.router.js
+++ app/scripts/routers/main.router.js
@@ -6,19 +6,41 @@
 
 
     initialize: function(){
+      Backbone.history.start();
       this.on('route', this.checkRoute, this);
     },
 
     routes: {
-      
+      'user_create': 'createUser',
+      'login': 'loginUser',
+      'list_games': 'listGames',
+      'game': 'gameRoute',
       '*path':  'defaultRoute'
 
+    },
+
+
+
+    createUser: function(){
+      app.createUser = new app.CreateUserView();
+    },
+
+    loginUser: function(){
+      app.loginUser = new app.UserLoginView();
+    },
+
+    listGames: function(){
+      
+    },
+
+    gameRoute: function () {
+      app.createGame = new app.BoardView();
     },
 
     defaultRoute: function () {
       this.navigate('', {trigger: true});
       console.log('Default');
-},
+    },
 
   });
 
